77e6104f664aed0bb8b6469be4c1f22ded5aca38
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-22 22:09:09 +0700

    problems with name

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
 	data: new SlashCommandBuilder()
-        .setName("Help")
+        .setName("help")
         .setDescription("Show the help info and commands!"),
 	execute: async ({ client, interaction }) => {
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nLeave: Leave the voice channel`).setColor(`Blue`)] })
