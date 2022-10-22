5979c745c32b46df05de21eb72f3aad8febc105d
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-22 21:55:02 +0700

    change pause to Help
    change description and name of interaction

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
 	data: new SlashCommandBuilder()
-        .setName("pause")
-        .setDescription("Pauses the current song"),
+        .setName("Help")
+        .setDescription("Show the help info and commands!"),
 	execute: async ({ client, interaction }) => {
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nLeave: Leave the voice channel`).setColor(`Blue`)] })
 	},
