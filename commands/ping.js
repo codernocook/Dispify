22860f991aed0451b6285356ca74ee841375bd90
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-22 21:28:43 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,8 +7,8 @@ module.exports = {
         .setDescription("Pong!"),
 	execute: async ({ client, interaction }) => {
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Pong! 0ms`).setColor(`Green`)] }).then(interactionget => {
-            const ping = interactionget.createdTimestamp - interactionget.createdTimestamp;
-            interactionget.editReply({ embeds: [new EmbedBuilder().setDescription(`Pong! ${ping}ms`).setColor(`Green`)] })
+            const ping = interactionget.createdTimestamp - interaction.createdTimestamp;
+            interaction.editReply({ embeds: [new EmbedBuilder().setDescription(`Pong! ${ping}ms`).setColor(`Green`)] })
         })
 	},
 }
