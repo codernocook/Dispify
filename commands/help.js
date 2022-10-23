8cb272f8c13f175b04c88d378a0e67a391d147f3
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-23 13:09:13 +0700

    new help command

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,6 @@ module.exports = {
         .setName("help")
         .setDescription("Show the help info and commands!"),
 	execute: async ({ client, interaction }) => {
-        interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nLeave: Leave the voice channel`).setColor(`Blue`)] })
+        interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nSkip [first/number) | First: Skip to next song | Number: Skip to track position\nLeave: Leave the voice channel`).setColor(`Blue`)] })
 	},
 }
