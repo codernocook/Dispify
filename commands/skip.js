428dc4114dbdc22e68d77d287929175970fbbecd
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-23 13:08:39 +0700

    added skip number and skip first

--- commands/skip.js
+++ commands/skip.js
@@ -4,32 +4,52 @@ const { EmbedBuilder } = require("discord.js")
 module.exports = {
 	data: new SlashCommandBuilder()
         .setName("skip")
-        .setDescription("Skips the current song"),
-
+        .setDescription("Skips the current song")
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("first")
+				.setDescription("Skip to next song.")
+		)
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("number")
+				.setDescription("Skip to track position.")
+				.addStringOption(option => option.setName("number").setDescription("Skip to track position.").setRequired(true))
+		),
 	execute: async ({ client, interaction }) => {
-
         // Get the queue for the server
 		const queue = client.player.getQueue(interaction.guildId)
 
         // If there is no queue, return
 		if (!queue)
         {
-            await interaction.reply({ embeds: [new EmbedBuilder().setDescription(`There are no song in the queue!`).setColor(`Red`)] })
+            await interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033601680387887125> There are no song in the queue!`).setColor(`Red`)] })
             return;
         }
 
         const currentSong = queue.current
 
-        // Skip the current song
-		queue.skip()
+        // Get subCommand of song
+        if (interaction.options.getSubcommand() === "number") {
+            let position = interaction.options.getString("number")
+            const queueSongNumber = queue.tracks.map((song, i) => {return i;})
 
-        // Return an embed to the user saying the song has been skipped
-        await interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                    .setDescription(`Skipped ${currentSong.title}!`)
-                    .setColor("Green")
-            ]
-        })
+            if (!position) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033601680387887125> Invaild Position, please type vaild position.`).setColor(`Red`)] });
+            if (!Number(position)) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033601680387887125> Invaild Position, please type vaild position.`).setColor(`Red`)] });
+            if ((Number(position)) > queueSongNumber) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033601680387887125> Invaild Position, please type vaild position.`).setColor(`Red`)] });
+
+            for (let loopskip = 0; loopskip < Number(position); loopskip++) {
+                queue.skip()
+            }
+
+            interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordSuccess:1033601653749862491> Skipped **[${currentSong.title}](${currentSong.url})**!`).setColor(`Green`)] })
+		}
+        else if (interaction.options.getSubcommand() === "first") {
+            // Skip the current song
+            queue.skip()
+
+            // Return an embed to the user saying the song has been skipped
+            interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordSuccess:1033601653749862491> Skipped **[${currentSong.title}](${currentSong.url})**!`).setColor(`Green`)] })
+        }
 	},
 }
