b1a6f0806b0d5f69e816e2866916e77e112e9776
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-23 21:34:29 +0700

    Fix my correction bruh and add website connection

--- commands/help.js
+++ commands/help.js
@@ -2,10 +2,10 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
-	data: new SlashCommandBuilder()
-        .setName("help")
-        .setDescription("Show the help info and commands!"),
-	execute: async ({ client, interaction }) => {
-        interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nSkip [first/number) | First: Skip to next song | Number: Skip to track position\nLeave: Leave the voice channel`).setColor(`Blue`)] })
-	},
+        data: new SlashCommandBuilder()
+                .setName("help")
+                .setDescription("Show the help info and commands!"),
+        execute: async ({ client, interaction }) => {
+                interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nSkip [first/number] | First: Skip to next song | Number: Skip to track position\nLeave: Leave the voice channel\nHelp page: https://spoticord.vercel.app/help/`).setColor(`Blue`)] })
+        },
 }
