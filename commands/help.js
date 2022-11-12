5e518a4a57965efc7839f56cfa8699742054d216
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-12 04:24:10 +0000

    New help and switch to version 2.0.0

--- commands/help.js
+++ commands/help.js
@@ -3,9 +3,9 @@ const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
         data: new SlashCommandBuilder()
-                .setName("help")
-                .setDescription("Show the help info and commands!"),
+        .setName("help")
+        .setDescription("Show the help info and commands!"),
         execute: async ({ client, interaction }) => {
-                interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command)\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist)\nPause: to pause current playing track.\nResume: continue play the current track\nPing: Get ping bot (Pong!)\nSkip [first/number] | First: Skip to next song | Number: Skip to track position\nLeave: Leave the voice channel\nHelp page: https://spoticord.vercel.app/help/`).setColor(`Blue`)] })
+                interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: / (use interaction command).\nPlay: [song/playlist/search] [track/playlist/search] (to play a track or playlist).\nPause: to pause current playing track.\nResume: continue play the current track.\nPing: Get ping bot (Pong!).\nSkip [first/number] | First: Skip to next song. | Number: Skip to track position.\nQueue [list/page] | List: Show First 10 songs in the queue. | Page: Turn to selected page in the list.\nLeave: Leave the voice channel.\nVersion: 2.0.0\nSupport Server: https://spoticord.vercel.app/invite/\nHelp page: https://spoticord.vercel.app/help/`).setColor(`Blue`)] })
         },
 }
