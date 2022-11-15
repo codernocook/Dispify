a8d9f811b0bac7226ef25c04945908e67163fcf6
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-15 09:50:47 +0000

    Fixed join

--- commands/join.js
+++ commands/join.js
@@ -17,14 +17,14 @@ module.exports = {
             });
         
             try {
-                await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+                entersState(connection, VoiceConnectionStatus.Ready, 30e3);
                 return connection;
             } catch (error) {
                 connection.destroy();
                 throw error;
             }
         }
-        const connection = await connectToChannel(interaction.member.voice.channel);
+        const connection = connectToChannel(interaction.member.voice.channel);
         connection.subscribe(createAudioPlayer()).catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't join the voice channel.`).setColor(`Red`)] })})
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordSuccess:1033721502874484746> Joining ${interaction.member.voice.channel.name}`).setColor(`Green`)] })
     },
