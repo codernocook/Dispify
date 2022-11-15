a1b348dee7e0183956c2f4866cc6482801862954
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-15 05:28:56 +0000

    fix join issue

--- commands/join.js
+++ commands/join.js
@@ -8,6 +8,6 @@ module.exports = {
 	execute: async ({ client, interaction }) => {
         if (!interaction.member.voice.channel) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033721529084694598> You must join a voice channel to make this command work.`).setColor(`Red`)] })
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordSuccess:1033721502874484746> Joining ${interaction.member.voice.channel.name}`).setColor(`Green`)] })
-        interaction.member.voice.channel.connect()
+        interaction.member.voice.channel.join().catch(err => console.error)
     },
 }
