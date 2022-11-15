9389e25772c2f36ca5e3b40cdfdb59dafc4aa25d
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-15 05:35:22 +0000

    fix dumb catch thing

--- commands/join.js
+++ commands/join.js
@@ -7,7 +7,8 @@ module.exports = {
         .setDescription("Make bot join your current voice channel."),
 	execute: async ({ client, interaction }) => {
         if (!interaction.member.voice.channel) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033721529084694598> You must join a voice channel to make this command work.`).setColor(`Red`)] })
+        if (!interaction.member.voice.channel.joinable) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033721529084694598> I can't join the voice channel.`).setColor(`Red`)] })
+        interaction.member.voice.channel.join().catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't join the voice channel.`).setColor(`Red`)] })})
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordSuccess:1033721502874484746> Joining ${interaction.member.voice.channel.name}`).setColor(`Green`)] })
-        interaction.member.voice.channel.join().catch(err => console.error)
     },
 }
