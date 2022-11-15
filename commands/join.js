eab1080a445c876508f6c460450d5572f807b96d
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-15 09:57:27 +0000

    Remove dumb play function and make joinvoicechannel work

--- commands/join.js
+++ commands/join.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { EmbedBuilder } = require("discord.js")
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus } = require("@discordjs/voice")
+const { joinVoiceChannel } = require("@discordjs/voice")
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,23 +9,11 @@ module.exports = {
 	execute: async ({ client, interaction }) => {
         if (!interaction.member.voice.channel) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033721529084694598> You must join a voice channel to make this command work.`).setColor(`Red`)] })
         if (!interaction.member.voice.channel.joinable) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordError:1033721529084694598> I can't join the voice channel.`).setColor(`Red`)] })
-        function connectToChannel(channel: VoiceChannel) {
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: createDiscordJSAdapter(channel),
-            });
-        
-            try {
-                entersState(connection, VoiceConnectionStatus.Ready, 30e3);
-                return connection;
-            } catch (error) {
-                connection.destroy();
-                throw error;
-            }
-        }
-        const connection = connectToChannel(interaction.member.voice.channel);
-        connection.subscribe(createAudioPlayer()).catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't join the voice channel.`).setColor(`Red`)] })})
+        joinVoiceChannel({
+            channelId: interaction.member.voice.channel.id,
+            guildId: interaction.guildId,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        }).catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't join the voice channel.`).setColor(`Red`)] })})
         interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:SpoticordSuccess:1033721502874484746> Joining ${interaction.member.voice.channel.name}`).setColor(`Green`)] })
     },
 }
