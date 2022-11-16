c12a6e463686f48ec22270d56be9f520ae0ac88c
codernocook <nhatlong.vn69@gmail.com>
2022-11-16 13:48:10 +0000

    Fixed Ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,13 +2,10 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
-	data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Pong!"),
-	execute: async ({ client, interaction }) => {
-        interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Pong! 0ms`).setColor(`Green`)] }).then(interactionget => {
-            const ping = interactionget.createdTimestamp - interaction.createdTimestamp;
-            interaction.editReply({ embeds: [new EmbedBuilder().setDescription(`Pong! ${ping}ms`).setColor(`Green`)] })
-        })
-	},
+        data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Show the help info and commands!"),
+        execute: async ({ client, interaction }) => {
+                interaction.reply({ embeds: [new EmbedBuilder().setDescription(`Pong! `${client.ws.ping}``).setColor(`Blue`)] })
+        },
 }
