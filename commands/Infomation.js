4bcbecbfe01c3e87610ec321d5df791e4cb73e51
codernocook <nhatlong.vn69@gmail.com>
2022-12-03 08:36:29 +0000

    Added Infomation!

---
+++ commands/Infomation.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { EmbedBuilder } = require("discord.js")
+const { QueryType } = require("discord-player")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("info")
+		.setDescription("Get current track infomation"),
+	execute: async ({ client, interaction }) => {
+        const queue = client.player.getQueue(interaction.guildId)
+
+        if (!queue || !queue.playing) {
+            await interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> There are no songs in the queue!`).setColor(`Red`)] });
+            return;
+        }
+        
+        const currentSong = queue.current
+
+        if (currentSong) {
+            interaction.reply({ embeds: [new EmbedBuilder().setTitle(currentSong.title).setThumbnail(currentSong.thumbnail).setDescription(`Author: ${currentSong.author}\nDescription: \`${currentSong.description}\`\nUrl: ${currentSong.url}`).setColor(`Blue`)] });
+        }
+	},
+}
