25a44004d7c54baba73b303feb0f296ce7b2e09b
codernocook <90613458+codernocook@users.noreply.github.com>
2022-12-12 08:53:20 -0600

    Last Update!
    It come to end, hope you still enjoy music and Dispify, we'll miss you. Goodbye!

---
+++ CommandDeployer.js
@@ -0,0 +1,30 @@
+// this is an script to make other work use deploy() function
+// deploy(commandinfo) to deploy
+// this module will make the bot command appear forever don't need old deploy method (anti deploy bypass)
+
+// require the eris client
+/* The bot deploy docs
+    const send = {
+        ["name"]
+        ["description"]
+        ["option"]
+    }
+*/
+
+const { CommandClient } = require('eris')
+const token = process.env.token // put the bot token here or change the env name
+const DeployerClient = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
+
+async function deployprocess(info) {
+    // Register the discord command, forever
+    DeployerClient.on('ready', async () => {
+        await DeployerClient.bulkEditCommands([info[0]])
+    })
+    DeployerClient.connect();
+}
+
+module.exports = {
+    deploy(info) {
+        deployprocess(info)
+    }
+}
