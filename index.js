a20a9ff82173e8f3cf8566f34ce17aa9301741f9
codernocook <90613458+codernocook@users.noreply.github.com>
2023-03-22 13:44:03 +0000

    Fixed track stop after 30s - 60s (not v6)
    The discord bot will rewrite soon during to discord-player v6 errors and bug.

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ client.on("ready", async () => {
     rest.put(Routes.applicationCommands(CLIENT_ID), {body: commands}).catch(err => console.log(err));
 });
 
+// My plan is upgrade to v6 but it have so many bugs so i use this method instead
+player.on('connectionCreate', (queue) => {
+    queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+      const oldNetworking = Reflect.get(oldState, 'networking');
+      const newNetworking = Reflect.get(newState, 'networking');
+
+      const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+        const newUdp = Reflect.get(newNetworkState, 'udp');
+        clearInterval(newUdp?.keepAliveInterval);
+      }
+
+      oldNetworking?.off('stateChange', networkStateChangeHandler);
+      newNetworking?.on('stateChange', networkStateChangeHandler);
+    });
+});
+
 client.player.on("trackStart", (queue, track) => queue.metadata.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Now Playing **[${track.title}](${track.url})**.`).setColor(`Green`)] }))
 
 client.player.on('trackEnd', async (queue, track) => {
