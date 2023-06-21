c1bfd4cf9e995cdd86a5a9070aae893f1b22d430
codernocook <shitseverok@gmail.com>
2023-06-21 05:25:07 +0100

    filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -20,43 +20,54 @@ module.exports = {
 	    	.setDescription("Clear/Remove all filter.")
 	),
     execute: async ({ client, interaction }) => {
-	// defer Reply (because ffmpeg take so much time to load)
-	await interaction.deferReply();
+		// defer Reply (because ffmpeg take so much time to load)
+		await interaction.deferReply();
 
-	// Get the current queue
-	const queue = client.player.nodes.get(interaction.guildId)
+		// Get the current queue
+		const queue = client.player.nodes.get(interaction.guildId)
 
-	if (!queue) {
-	    await interaction.editReply({
-		embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> There are no song in the queue!`).setColor(`Red`)]
-	    })
-	    return;
-	}
+		if (!queue) {
+			await interaction.editReply({
+				embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> There are no song in the queue!`).setColor(`Red`)]
+			})
+			return;
+		}
 
-	// Add filter
-	if (interaction.options.getSubcommand() === "set") {
-	    let option = interaction.options.getString("name").toLowerCase(); // the option for set command
-	    try {
-		await queue.filters.ffmpeg.toggle([option?.toString()]);
-		await interaction.editReply({
-		    embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Added filter **${option?.toString()}** to the playlist.`).setColor(`Green`)]
-		});
-	    } catch {
-		await interaction.editReply({
-		    embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Cannot set this filter, make sure the **ffmpeg** filter is vaild.\nTheir documentation: https://ffmpeg.org/ffmpeg-filters.html`).setColor(`Red`)]
-		});
-	    }
-	} else if (interaction.options.getSubcommand() === "clear") {
-	    try {
-		await queue.filters.ffmpeg.toggle([]); // clear is toggle nothing so this should be blank
-		await interaction.editReply({
-		    embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Successfully removed all filter in the list.`).setColor(`Green`)]
-		});
-	    } catch {
-		await interaction.editReply({
-		    embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Something went wrong, we can't clear the filter list.`).setColor(`Red`)]
-		});
-	    }
-	}
+		// Add filter
+		if (interaction.options.getSubcommand() === "set") {
+			let option = interaction.options.getString("name").toLowerCase(); // the option for set command
+			try {
+				await queue.filters.ffmpeg.toggle([option?.toString()]).then(() => {
+					interaction.editReply({
+						embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Added filter **${option?.toString()}** to the playlist.`).setColor(`Green`)]
+					});
+				}).catch(() => {
+					interaction.editReply({
+						embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Cannot set this filter, make sure the **ffmpeg** filter is vaild.\nTheir documentation: https://ffmpeg.org/ffmpeg-filters.html`).setColor(`Red`)]
+					});
+				});
+			} catch {
+				await interaction.editReply({
+					embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Cannot set this filter, make sure the **ffmpeg** filter is vaild.\nTheir documentation: https://ffmpeg.org/ffmpeg-filters.html`).setColor(`Red`)]
+				});
+			}
+		} else if (interaction.options.getSubcommand() === "clear") {
+			try {
+				// clear is toggle nothing so this should be blank
+				await queue.filters.ffmpeg.toggle([]).catch(() => {
+					interaction.editReply({
+						embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Successfully removed all filter in the list.`).setColor(`Green`)]
+					});
+				}).catch(() => {
+					interaction.editReply({
+						embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Something went wrong, we can't clear the filter list.`).setColor(`Red`)]
+					});
+				});
+			} catch {
+				await interaction.editReply({
+					embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Something went wrong, we can't clear the filter list.`).setColor(`Red`)]
+				});
+			}
+		}
     },
 }
