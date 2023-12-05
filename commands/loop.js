0f4981964336ae95092b545852529801ff4cbc11
codernocook <90613458+codernocook@users.noreply.github.com>
2023-12-05 12:31:23 +0000

    Fixed loop.js
    - Better options
    - Working!

--- commands/loop.js
+++ commands/loop.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { EmbedBuilder } = require("discord.js")
-const { QueueRepeatMode, useQueue } = require('discord-player');
+const { QueueRepeatMode } = require('discord-player');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,7 +11,7 @@ module.exports = {
                 .setName("on")
                 .setDescription("Set loop mode to true.")
                 .addStringOption(option =>
-                    option.setName("type").setDescription("There are three types: track, queue, autoplay").setRequired(true)
+                    option.setName("type").setDescription("There are three types: track/song, queue, autoplay. Default the option is \"track\"").setRequired(false)
                 )
         )
         .addSubcommand(subcommand =>
@@ -20,11 +20,11 @@ module.exports = {
                 .setDescription("Set loop mode to false.")
         ),
 	execute: async ({ client, interaction }) => {
-        // Make sure the user is inside a voice channel
-		if (!interaction.member.voice.channel) return interaction.reply({ embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> You must join a voice channel to play a track!`).setColor(`Red`)] });
-
         // Defer reply to prevent many command make bot crash
         await interaction.deferReply();
+
+        // Make sure the user is inside a voice channel
+		if (!interaction.member.voice.channel) return interaction.editReply({ embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> You must join a voice channel to use the command.`).setColor(`Red`)] });
 
         // Get the current queue
 		const queue = client.player.nodes.get(interaction.guildId)
@@ -39,21 +39,27 @@ module.exports = {
         // Loop
         if (interaction.options.getSubcommand() === "on") {
             // Check type
-            let typeof_loopMode = interaction.options.getString("type").toLowerCase() || "track";
-            let loopMode_parsed = "track";
+            let typeof_loopMode = interaction.options.getString("type")?.toString().toLowerCase().replace(" ", "") || "track";
+            let loopMode_parsed = QueueRepeatMode.TRACK;
 
             // Support older js version (Traditional method)
             switch(typeof_loopMode) {
                 case "track": loopMode_parsed = QueueRepeatMode.TRACK; break;
+                case "song": loopMode_parsed = QueueRepeatMode.TRACK; break;
+                case "track/song": loopMode_parsed = QueueRepeatMode.TRACK; break;
                 case "queue": loopMode_parsed = QueueRepeatMode.QUEUE; break;
                 case "autoplay": loopMode_parsed = QueueRepeatMode.AUTOPLAY; break;
+                default:
+                    return interaction.editReply({
+                        embeds: [new EmbedBuilder().setDescription(`<:DispifyError:1033721529084694598> Not a vaild type of loop.`).setColor(`Red`)]
+                    });
             }
             
-            if (queue && queue["metadata"] && queue["metadata"]["looped"] === false) {
+            if (queue && queue["metadata"] && queue["metadata"]["looped"] === false && queue["metadata"]["loopMode"] !== loopMode_parsed) {
                 queue["metadata"]["looped"] = true;
-                queue.setRepeatMode(typeof_loopMode);
+                queue.setRepeatMode(loopMode_parsed);
                 interaction.editReply({
-                    embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Set the loop mode to \`on\`.`).setColor(`Green`)]
+                    embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Set the loop mode to \`on\`, type: \`${typeof_loopMode}\`.`).setColor(`Green`)]
                 });
             } else {
                 interaction.editReply({
@@ -61,11 +67,11 @@ module.exports = {
                 });
             }
         } else if (interaction.options.getSubcommand() === "off") {
-            if (queue && queue["metadata"] && queue["metadata"]["looped"] === true) {
+            if (queue && queue["metadata"] && queue["metadata"]["looped"] === true && queue["metadata"]["loopMode"] !== loopMode_parsed) {
                 queue["metadata"]["looped"] = false;
                 queue.setRepeatMode(QueueRepeatMode.OFF);
                 interaction.editReply({
-                    embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Set the loop mode to \`off\`.`).setColor(`Green`)]
+                    embeds: [new EmbedBuilder().setDescription(`<:DispifySuccess:1033721502874484746> Set the loop mode to \`off\`, type: \`${typeof_loopMode}\`.`).setColor(`Green`)]
                 });
             } else {
                 interaction.editReply({
